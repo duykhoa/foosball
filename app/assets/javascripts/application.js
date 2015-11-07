d2a7928c879ba1355639406bf9df10913b37fd45
Duy Khoa <duykhoa12t@gmail.com>
2015-11-07 22:01:25 +0800

    Extract Booty object to booty.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,20 +16,4 @@
 //= require_tree .
 //
 
-Booty = {
-  init: function() {
-    Booty.sideNav();
-  },
-  sideNav: function() {
-    $('.button-collapse').sideNav({
-          menuWidth: 300, // Default is 240
-          edge: 'right', // Choose the horizontal origin
-          closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        }
-      );
-  }
-}
-
-window.Booty = Booty;
-
 $(document).ready(Booty.init);
---
+++ app/assets/javascripts/booty.js
@@ -0,0 +1,15 @@
+Booty = {
+  init: function() {
+    Booty.sideNav();
+  },
+  sideNav: function() {
+    $('.button-collapse').sideNav({
+          menuWidth: 300, // Default is 240
+          edge: 'right', // Choose the horizontal origin
+          closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        }
+      );
+  }
+}
+
+window.Booty = Booty;
