2f9e8d1c73dbd59717c382ce13f400906b87db79
Duy Khoa <duykhoa12t@gmail.com>
2015-11-08 16:07:23 +0800

    Add validator for team name

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,7 @@ class Team < ActiveRecord::Base
   has_and_belongs_to_many :users
 
   validates_length_of :users, maximum: 2
+  validates :name, presence: true
 
   def to_s
     name
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Team, type: :model do
   it { should have_and_belong_to_many(:users) }
+  it { should validate_presence_of(:name) }
 
   describe "#to_s" do
     let(:team) { FactoryGirl.build(:team, name: "Team#1") }
