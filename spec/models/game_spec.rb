0c85e5041d01ab23517fa2209c1c8cb9f3b9849a
Duy Khoa <duykhoa12t@gmail.com>
2015-11-08 23:45:56 +0800

    Validate score_team1 & 2 with range 0-10

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Game, type: :model do
   it { should belong_to(:team1).class_name(Team).with_foreign_key("team1_id") }
   it { should belong_to(:team2).class_name(Team).with_foreign_key("team2_id") }
   it { should validate_presence_of(:team1) }
+  it { should validate_numericality_of(:score_team1).with_message("Score must be in 0-10") }
+  it { should validate_numericality_of(:score_team2).with_message("Score must be in 0-10") }
 end
