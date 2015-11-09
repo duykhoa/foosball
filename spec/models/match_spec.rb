f6b3c7a3d3b71dc3621625545b80ffa68e0d947a
Duy Khoa <duykhoa12t@gmail.com>
2015-11-09 16:35:28 +0800

    Add shoulda test for match model

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Match, type: :model do
   it { should have_and_belong_to_many(:games) }
   it { should belong_to(:team1).class_name(Team).with_foreign_key("team1_id") }
   it { should belong_to(:team2).class_name(Team).with_foreign_key("team2_id") }
+
+  it { should validate_length_of(:games) }
 end
