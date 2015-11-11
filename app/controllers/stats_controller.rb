04e6a0a6dc718112e845bc52ce4c9a2481dfe33f
Duy Khoa <duykhoa12t@gmail.com>
2015-11-11 10:37:14 +0800

    Add more stat cards to stat#index page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,7 +2,11 @@ class StatsController < ApplicationController
   def index
     @best_team_stat = Stat.new.best_team
     @best_team = Team.find @best_team_stat.first.winning_team_id
-    @count_win_for_users = Stat.new.count_win
-    @total_game_for_users = Stat.new.total_game
+    @count_win_for_users = hash_mapping(Stat.new.count_win)
+    @total_game_for_users = hash_mapping(Stat.new.total_game)
+  end
+
+  def hash_mapping(result)
+    result.inject({}) { |r, i| r[i.user_id] = i; r }
   end
 end
