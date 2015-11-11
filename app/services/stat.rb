85835e87c8a33c8992261fb713858fceb2dce436
Duy Khoa <duykhoa12t@gmail.com>
2015-11-11 09:51:56 +0800

    Add user_name to count_win & total_game method

--- app/services/stat.rb
+++ app/services/stat.rb
@@ -10,7 +10,7 @@ class Stat
 
   def count_win
     User.find_by_sql("\
-      SELECT users.id, count(users.id) as num_wins, teams_users.* \
+      SELECT users.id, users.first_name, count(users.id) as num_wins, teams_users.* \
       FROM users \
         INNER JOIN teams_users ON teams_users.user_id = users.id \
         INNER JOIN games ON games.winning_team_id = teams_users.team_id \
@@ -21,7 +21,7 @@ class Stat
 
   def total_game
     User.find_by_sql("\
-      SELECT users.id, count(users.id) as total_games, teams_users.* \
+      SELECT users.id, users.first_name, count(users.id) as total_games, teams_users.* \
       FROM users \
         INNER JOIN teams_users ON teams_users.user_id = users.id \
         INNER JOIN games ON games.team1_id = teams_users.team_id OR games.team2_id = teams_users.team_id \
