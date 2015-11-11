accb5c43bede5af1220851f4d0529e5bd78fabeb
Duy Khoa <duykhoa12t@gmail.com>
2015-11-11 21:43:38 +0800

    Add condition for stats controller

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,9 @@
 class StatsController < ApplicationController
   def index
     @best_team_stat = Stat.new.best_team
-    @best_team = Team.find @best_team_stat.first.winning_team_id
+    if @best_team_stat.length > 0
+      @best_team = Team.find @best_team_stat.first.winning_team_id
+    end
     @count_win_for_users = hash_mapping(Stat.new.count_win)
     @total_game_for_users = hash_mapping(Stat.new.total_game)
   end
